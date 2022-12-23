96566c6d0ff731af2512f1fec0f920e38f703d54
ilyass atlassi <atlassiilyass@gmail.com>
2022-12-23 16:29:45 +0100

    Add the script for the project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import './style.css';
+const list = document.getElementById('listtodo')
+let completed = false,
+todoList = [
+  {
+    "description": "wash the dishes",
+    "completed" : completed,
+    "index" : 0
+  },
+  {
+    "description": "wash the dishes",
+    "completed" : completed,
+    "index" : 1
+  },
+];
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+function showTodo() {
+  let litItems = "";
+  todoList.forEach((element) => 
+      
+          litItems += `<li class="task">
+                                  <div>
+                                  <input type="checkbox" class="checkme" id="check${element.index}">
+                                  <input type="text" value="${element.description}" class="listitem" id="item${element.index}" readonly>
+                                  </div>
+                                  <div>
+                                  <i class="fa-solid fa-pen-to-square edit " id="edit${element.index}" onclick="editItem(${element.index});"></i>
+                                  <i class="fa-solid fa-floppy-disk save hide" id="save${element.index}" onclick="saveItem(${element.index});"></i>
+                                  <i id="removeicon" onclick="removeItem(${element.index});" class="fa-solid fa-trash"></i>
+                                  </div>
+                                  
+                          </li>`
+);
+  list.innerHTML = litItems ;
+}
+
+showTodo();
