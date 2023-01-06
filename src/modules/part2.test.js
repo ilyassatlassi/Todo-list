1baa1f31731d85478b10a496d39cd68d85f2e2f7
Athaliah <athaliahtauro@gmail.com>
2023-01-06 23:02:19 +0200

    Fix linter issues

--- src/modules/part2.test.js
+++ src/modules/part2.test.js
@@ -1,81 +1,78 @@
-/* eslint-disable */
+/* eslint-disable  */
 const todoList = require('./main.js');
 const list = document.getElementById('listtodo');
-const inputAdd = document.querySelector(".input input");
+const inputAdd = document.querySelector('.input input');
 
-let completed = false;
-let index = 0;
-// let todoList = [];
+const completed = false;
+const index = 0;
 
 // Mocking localStorage object
 const mockStorage = {
-todoList: [],
-setItem: (key, value) => {
-mockStorage[key] = value;
-},
-getItem: (key) => mockStorage[key],
-removeItem: (key) => {
-delete mockStorage[key];
-}
-}
+  todoList: [],
+  setItem: (key, value) => {
+    mockStorage[key] = value;
+  },
+  getItem: (key) => mockStorage[key],
+  removeItem: (key) => {
+    delete mockStorage[key];
+  },
+};
 
 // Mocking DOM elements
 const mockList = {
-innerHTML: '',
-set innerHTML(html) {
-this.innerHTML = html;
-},
-get innerHTML() {
-return this.innerHTML;
-}
-}
+  innerHTML: '',
+  set innerHTML(html) {
+    this.innerHTML = html;
+  },
+  get innerHTML() {
+    return this.innerHTML;
+  },
+};
 
 const mockInput = {
-value: '',
-set value(value) {
-this.value = value;
-},
-get value() {
-return this.value;
-}
-}
+  value: '',
+  set value(value) {
+    this.value = value;
+  },
+  get value() {
+    return this.value;
+  },
+};
 
 // Test group for editFunc function
 describe('editFunc function', () => {
-it('should enable editing of a task when called', () => {
-// Arrange
-const mockTask = {
-index: 0,
-description: 'Buy milk',
-completed: false
-}
-todoList = [mockTask];
-mockStorage.todoList = [mockTask];
-showList();
-const mockEditButton = document.getElementById('edit0');
-const mockSaveButton = document.getElementById('save0');
-const mockInputField = document.getElementById('item0');
-// Act
-window.editFunc(0);
+  it('should enable editing of a task when called', () => {
+    // Arrange
+    const mockTask = {
+      index: 0,
+      description: 'Buy milk',
+      completed: false,
+    };
+    todoList = [mockTask];
+    mockStorage.todoList = [mockTask];
+    showList();
+    const mockEditButton = document.getElementById('edit0');
+    const mockSaveButton = document.getElementById('save0');
+    const mockInputField = document.getElementById('item0');
+    // Act
+    window.editFunc(0);
 
-// Assert
-expect(mockEditButton.style.display).toBe('none');
-expect(mockSaveButton.style.display).toBe('flex');
-expect(mockInputField.readOnly).toBe(false);
-
-
-});
+    // Assert
+    expect(mockEditButton.style.display).toBe('none');
+    expect(mockSaveButton.style.display).toBe('flex');
+    expect(mockInputField.readOnly).toBe(false);
+  });
 });
 
 // Test group for saveFunc function
 describe('saveFunc function', () => {
-    it('should save the edited task when called', () => {
+  it('should save the edited task when called', () => {
     // Arrange
     const mockTask = {
-    index: 0,
-    description: 'Buy milk',
-    completed: false
-    }
+      index: 0,
+      description: 'Buy milk',
+      completed: false,
+    };
     todoList = [mockTask];
     mockStorage.todoList = [mockTask];
     showList();
@@ -83,35 +80,35 @@ describe('saveFunc function', () => {
     const mockSaveButton = document.getElementById('save0');
     const mockInputField = document.getElementById('item0');
     mockInputField.value = 'Buy 2% milk';
-    
-    // Act
-window.saveFunc(0);
 
-// Assert
-expect(mockEditButton.style.display).toBe('flex');
-expect(mockSaveButton.style.display).toBe('none');
-expect(mockInputField.readOnly).toBe(true);
-});
+    // Act
+    window.saveFunc(0);
+
+    // Assert
+    expect(mockEditButton.style.display).toBe('flex');
+    expect(mockSaveButton.style.display).toBe('none');
+    expect(mockInputField.readOnly).toBe(true);
+  });
 });
 // Test group for checkFunc function
 describe('checkFunc function', () => {
-    it('should update the "completed" status of a task when called', () => {
+  it('should update the "completed" status of a task when called', () => {
     // Arrange
     const mockTask = {
-    index: 0,
-    description: 'Buy milk',
-    completed: false
-    }
+      index: 0,
+      description: 'Buy milk',
+      completed: false,
+    };
     todoList = [mockTask];
     mockStorage.todoList = [mockTask];
     showList();
     const mockCheckbox = document.getElementById('check0');
     // Act
-window.checkFunc(0);
+    window.checkFunc(0);
 
-// Assert
-expect(todoList[0].completed).toBe(true);
-expect(mockStorage.todoList[0].completed).toBe(true);
-expect(mockCheckbox.checked).toBe(true);
-});
+    // Assert
+    expect(todoList[0].completed).toBe(true);
+    expect(mockStorage.todoList[0].completed).toBe(true);
+    expect(mockCheckbox.checked).toBe(true);
+  });
 });
